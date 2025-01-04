b3fcf8e465e6b65eaf254727149f1c289682c0b8
김기진 <abjin69@gmail.com>
2025-01-04 15:47:18 +0900

    import http module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    { ...HttpModule.register({}), global: true },
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
