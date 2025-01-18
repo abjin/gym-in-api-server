1457aa3500adc8c0ee6f7f29353c780575b9d3b0
김기진 <abjin69@gmail.com>
2025-01-18 15:02:16 +0900

    feat(redis): delete async

--- db/redis.service.ts
+++ db/redis.service.ts
@@ -7,8 +7,7 @@ const REDIS_CLIENT = Symbol('REDIS_CLIENT');
 export const RedisService = {
   provide: REDIS_CLIENT,
   inject: [ConfigService],
-  useFactory: async (config: ConfigService) =>
-    new Redis(config.get('REDIS_URL')),
+  useFactory: (config: ConfigService) => new Redis(config.get('REDIS_URL')),
 };
 
 export const InjectRedis = (): ParameterDecorator => Inject(REDIS_CLIENT);
