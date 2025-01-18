9281c1e8a6a6b43ee683ef4fe8295df10b8a2d27
김기진 <abjin69@gmail.com>
2025-01-18 14:45:59 +0900

    feat(redis): delete redis ping pong test endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { InjectRedis } from 'db/redis.service';
-import Redis from 'ioredis';
 
 @Controller()
 export class AppController {
-  constructor(@InjectRedis() private readonly client: Redis) {}
-
   @Get()
   healthCheck() {
     return { uptime: process.uptime(), ...process.memoryUsage() };
-  }
-
-  @Get('redis-ping')
-  redisPingTest() {
-    return this.client.ping();
   }
 }
