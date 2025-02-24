15780402050f82b7789d44e9f5b38ac057c31e84
김기진 <abjin69@gmail.com>
2025-02-24 16:20:21 +0900

    feat(date): Add start and end of week utility methods

--- libs/date/src/date.service.ts
+++ libs/date/src/date.service.ts
@@ -2,9 +2,15 @@ import { Injectable } from '@nestjs/common';
 import * as dayjs from 'dayjs';
 import * as utc from 'dayjs/plugin/utc';
 import * as timezone from 'dayjs/plugin/timezone';
+import * as isBetween from 'dayjs/plugin/isBetween';
+import * as weekOfYear from 'dayjs/plugin/weekOfYear';
+import * as isoWeek from 'dayjs/plugin/isoWeek';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.extend(isBetween);
+dayjs.extend(weekOfYear);
+dayjs.extend(isoWeek);
 
 const defaultTimeZone = 'Asia/Seoul';
 
@@ -37,5 +43,21 @@ export class DateService {
     format = 'YYYY-MM-DD',
   ): string {
     return dayjs(date).tz(timeZone).endOf('month').format(format);
+  }
+
+  static getStartOfWeekString(
+    date: Date | string = new Date(),
+    timeZone: string = defaultTimeZone,
+    format = 'YYYY-MM-DD',
+  ): string {
+    return dayjs(date).tz(timeZone).startOf('isoWeek').format(format);
+  }
+
+  static getEndOfWeekString(
+    date: Date | string = new Date(),
+    timeZone: string = defaultTimeZone,
+    format = 'YYYY-MM-DD',
+  ): string {
+    return dayjs(date).tz(timeZone).endOf('isoWeek').format(format);
   }
 }
